407dddbf5d27d5c4f968550aa401717dbf35ae27
Salvador Alejandro Pineda Vera <24470244@campeche.tecnm.mx>
2025-12-11 15:18:12 -0600

    Agregacion de los paquetes para SQL

---
+++ ShopList.Gui/Persistence/Configuration/Constants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopList.Gui.Persistence.Configuration
+{
+    public static class Constants
+    {
+        public const string DataBaseFileName = "ShoplistDb.db3";
+        public static string DataBasePath
+            => Path.Combine(FileSystem.AppDataDirectory, DataBaseFileName);
+        public const SQLite.SQLiteOpenFlags Flags = 
+            SQLite.SQLiteOpenFlags.ReadWrite | 
+            SQLite.SQLiteOpenFlags.Create | 
+            SQLite.SQLiteOpenFlags.SharedCache;
+    }
+}
